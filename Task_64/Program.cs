f90c3d79f6c989e45c545dc0492f650522d40954
Sergey <u3jiom@bk.ru>
2022-11-18 19:47:18 +0400

    Решение задач

---
+++ Task_64/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void NaturalNumbers (int num) 
+{
+    if (num > 0) return;
+    Console.Write($"{0} ", num);
+    NaturalNumbers(num - 1);
+
+}
+
+NaturalNumbers(number);
